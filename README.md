a4b17eed92f7684f1138f6287d33046de327b8a8
Mark Miro <contact@markmiro.com>
2022-12-18 20:55:00 -0800

    Generate site from data files

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
 # README
 
-Hosted on: https://migratingcranes.com
+Data from:
+https://drive.google.com/drive/folders/1rQ09Cd04krH0k50cewVERfOJfXGdrYTo?usp=share_link
+
+And then placed in `/data/us` directory (for reports of immigration to the US).
+
+Hosted on:
+https://migratingcranes.com
 
 Favicon from:
 https://favicon.io/emoji-favicons/feather/
+
+# Developing
+
+After pulling the repo locally...
+
+Install the dependencies
+
+```sh
+npm install
+```
+
+Build the file
+```sh
+npm run build
+```
+
+Check locally
+```sh
+npm run start
+```
 
 # Tech stack
 
---
+++ build.mjs
@@ -0,0 +1,20 @@
+import fse from "fs-extra";
+import Handlebars from "handlebars";
+import slugify from "slugify";
+import _ from "lodash";
+
+const source = fse.readFileSync("public/index.html.handlebars", "utf8");
+const template = Handlebars.compile(source);
+
+const files = fse.readdirSync("data/us");
+const COUNT = 3; // Number of countries to show on the homepage (for now)
+
+const countries = _.take(files, COUNT).map((file) => {
+  const name = file.replace("_Report_Prototype.html", "").replace(", The", "");
+  const slug = slugify(name, { lower: true, remove: /'/g });
+  fse.cpSync(`data/us/${file}`, `public/to/us/from/${slug}.html`);
+  return { name, slug };
+});
+
+const res = template({ countries });
+fse.writeFileSync("public/index.html", res);
