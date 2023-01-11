8e15b56ac564c27b7b2671d215d448ca58c9be0b
Mark Miro <contact@markmiro.com>
2023-01-10 20:35:53 -0800

    Update build script

--- build.mjs
+++ build.mjs
@@ -7,11 +7,11 @@ const source = fse.readFileSync("public/index.html.handlebars", "utf8");
 const template = Handlebars.compile(source);
 
 const files = fse.readdirSync("data/us");
-const COUNT = 3; // Number of countries to show on the homepage (for now)
+const COUNT = files.length; // Number of countries to show on the homepage (for now)
 
 const countries = _.take(files, COUNT).map((file) => {
   const name = file.replace("_Report_Prototype.html", "").replace(", The", "");
-  const slug = slugify(name, { lower: true, remove: /'/g });
+  const slug = slugify(name, { lower: true, remove: /[',]/g });
   fse.cpSync(`data/us/${file}`, `public/to/us/from/${slug}.html`);
   return { name, slug };
 });
